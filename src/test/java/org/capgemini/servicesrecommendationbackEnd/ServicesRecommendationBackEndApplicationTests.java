567e8ea664fe55e82fa164493eaf4924e1a70f39
ZEDDOU <soufiane.a.zeddou@capgemini.com>
2022-05-16 16:26:01 +0100

    Project Initialization

---
+++ src/test/java/org/capgemini/servicesrecommendationbackEnd/ServicesRecommendationBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.capgemini.servicesrecommendationbackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesRecommendationBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
