704834b485a5606643356a358d68efcf3a169ed1
CHADLI <zakaria.chadli@capgemini.com>
2022-05-17 12:27:39 +0100

    add layer models

---
+++ src/main/java/org/capgemini/servicesrecommendationbackEnd/models/User.java
@@ -0,0 +1,30 @@
+package org.capgemini.servicesrecommendationbackEnd.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Long idUser;
+    private String email;
+    private String pseudo;
+    private String username;
+    private String password;
+    @ManyToMany
+    @JoinTable(name="users_roles",
+        joinColumns = @JoinColumn(name = "id_user"),
+        inverseJoinColumns = @JoinColumn(name="id_role")
+    )
+    private List<Role> roles;
+}
